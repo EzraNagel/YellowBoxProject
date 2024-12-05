88090abad757e291a6118252c4dc8f54977a74d8
EzraNagel <ezra.bagel@gmail.com>
2024-12-04 22:03:22 -0500

    Database connection

--- yellowbox/config.py
+++ yellowbox/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin:yellowboxpassword$@yellowbox-db.c12ociym6hbt.us-east-1.rds.amazonaws.com:3306/yellowbox_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://user:yellowboxpassword@yellowbox-db.c12ociym6hbt.us-east-1.rds.amazonaws.com:3306/yellowbox_db'
 
 
 
