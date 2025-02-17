b9d97594968f89f2d99355394dca4507961e8825
EzraNagel <ezra.bagel@gmail.com>
2025-02-17 13:43:08 -0500

    Removed config.py from tracking

--- yellowbox/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin:yellowboxpassword$@yellowbox-db.c12ociym6hbt.us-east-1.rds.amazonaws.com:3306/yellowbox_db'
